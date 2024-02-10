584ca8b5b262576b6bb99ba9c9627fc0672f0e94
Utkarsh <43597586+utkarshs106@users.noreply.github.com>
2024-02-10 19:48:26 +0530

    Patreon Addec

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@
 public class Main {
     public static void main(String[] args) {
     Book b1 = new Book.BookBuilder().setISBN(1).setAvailability("Available").setGenere("Genere1").setTitle("Title32").setAuthor("Utkarsh ").Build();
+    Patreon p1 = new Patreon.PatreonBuilder().setPID(1).setPName("Rohit").setUserName("BlackHat").setPassword("89432").setMembershipStatus("Active").Build();
+    p1.setAmount(10090);
+    p1.setPName("Rohit Shukla");
+    System.out.println(p1);
+    System.out.println(p1.getPName());
     System.out.println(b1.getAuthor());
     }
 }
